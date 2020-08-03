97b2cf157109a119f1ddbab0952ed08ab3555c12
Aleman Juan <juan.aleman@aldeasos.org.ni>
2020-08-03 15:42:07 -0600

    Configuración inicial de clases y rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
+import { OrdersComponent } from './orders/orders.component';
+import { OrderComponent } from './orders/order/order.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {path: '', redirectTo: 'order', pathMatch: 'full'},
+  {path: 'orders', component:OrdersComponent},
+  {path: 'order', children:[
+    {path: '', component:OrderComponent},
+    {path: 'edit/:id', component:OrderComponent}
+
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
